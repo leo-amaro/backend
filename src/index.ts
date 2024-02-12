e91fefe02e49faa2b0bd9d3d503a71e721015a36
leo-amaro-conestoga <lamaro3722@conestogac.on.ca>
2024-02-12 14:05:09 -0500

    backend update

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import userRoutes from "./routes/users";
-import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -12,8 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
+app.get("/api/test", async (req, res) => {
+  res.json({ message: "test endpoint" });
+});
 
 app.listen(7000, () => {
   console.log("server is running");
